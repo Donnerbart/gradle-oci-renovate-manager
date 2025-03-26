01c1a2980d053840acb85d0095c712d5907c6e4d
David Sondermann <david.sondermann@hivemq.com>
2025-03-26 17:20:17 +0100

    Fix matcher

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ oci {
             imageTag = provider { project.version.toString().lowercase() }
             allPlatforms {
                 dependencies {
-                    runtime("library:alpine:sha256!56fa17d2a7e7f168a043a2712e63aed1f8543aeafdcee47c58dcffe38ed51099") // 3.21.2
+                    runtime("library:alpine:sha256!56fa17d2a7e7f168a043a2712e63aed1f8543aeafdcee47c58dcffe38ed51099")
                 }
                 config {
                     configAnnotations = mapOf(
